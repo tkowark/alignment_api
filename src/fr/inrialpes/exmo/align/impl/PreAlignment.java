282c0fed628e9ac6e778a246b7970966037c5fc0
pierson <pierson@a7222439-140d-0410-8486-c7398dd066c2>
2004-08-18 13:19:06 +0000

    *** empty log message ***
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@51 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/impl/PreAlignment.java
+++ src/fr/inrialpes/exmo/align/impl/PreAlignment.java
@@ -4,7 +4,7 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-package classes.fr.inrialpes.exmo.align.impl;
+package fr.inrialpes.exmo.align.impl;
 
 /**
  * @author jpierson
