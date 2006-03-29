41e05bf2d7652b5a6fab3789257403f0b072b210
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2006-03-29 13:29:32 +0000

    Simple Metrics for Ontology Alignment
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@230 a7222439-140d-0410-8486-c7398dd066c2

---
+++ src/fr/inrialpes/exmo/align/impl/method/SMOANameAlignment.java
@@ -0,0 +1,82 @@
+/*
+ * $Id: EditDistNameAlignment.java 153 2005-06-21 15:44:02Z euzenat $
+ *
+ * Copyright (C) INRIA Rhône-Alpes, 2003-2006
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package fr.inrialpes.exmo.align.impl.method; 
+
+import java.util.Iterator;
+
+import org.semanticweb.owl.model.OWLOntology;
+import org.semanticweb.owl.model.OWLClass;
+import org.semanticweb.owl.model.OWLProperty;
+import org.semanticweb.owl.model.OWLIndividual;
+import org.semanticweb.owl.model.OWLException;
+
+import org.semanticweb.owl.align.Alignment;
+import org.semanticweb.owl.align.AlignmentProcess;
+import org.semanticweb.owl.align.Cell;
+import org.semanticweb.owl.align.AlignmentException;
+import org.semanticweb.owl.align.Parameters;
+
+import fr.inrialpes.exmo.align.impl.DistanceAlignment;
+import fr.inrialpes.exmo.align.impl.MatrixMeasure;
+
+/**
+ * This class aligns ontology with regard to the editing distance between 
+ * class names.
+ *
+ * @author Jérôme Euzenat
+ * @version $Id: EditDistNameAlignment.java 153 2005-06-21 15:44:02Z euzenat $ 
+ */
+
+public class SMOANameAlignment extends DistanceAlignment implements AlignmentProcess
+{
+    /** Creation **/
+    public SMOANameAlignment( OWLOntology onto1, OWLOntology onto2 ){
+	super( onto1, onto2 );
+	setSimilarity( new MatrixMeasure() {
+		public double measure( OWLClass cl1, OWLClass cl2 ) throws OWLException{
+		    String s1 = cl1.getURI().getFragment();
+		    String s2 = cl2.getURI().getFragment();
+		    if ( s1 == null || s2 == null ) return 1.;
+		    else return StringDistances.smoaDistance(
+							s1.toLowerCase(),
+							s2.toLowerCase());
+		}
+		public double measure( OWLProperty pr1, OWLProperty pr2 ) throws OWLException{
+		    String s1 = pr1.getURI().getFragment();
+		    String s2 = pr2.getURI().getFragment();
+		    if ( s1 == null || s2 == null ) return 1.;
+		    else return StringDistances.smoaDistance(
+							s1.toLowerCase(),
+							s2.toLowerCase());
+		}
+		public double measure( OWLIndividual id1, OWLIndividual id2 ) throws OWLException{
+		    String s1 = id1.getURI().getFragment();
+		    String s2 = id2.getURI().getFragment();
+		    if ( s1 == null || s2 == null ) return 1.;
+		    else return StringDistances.smoaDistance(
+							s1.toLowerCase(),
+							s2.toLowerCase());
+		}
+	    } );
+    };
+
+}
+
