feb83c8ba698667fe3387e6e53ab32e5bf2acd9f
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2006-09-05 12:11:45 +0000

    - Added message for exchanging with protocol
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@319 a7222439-140d-0410-8486-c7398dd066c2

---
+++ src/fr/inrialpes/exmo/align/service/Message.java
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) INRIA Rhône-Alpes, 2006
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package fr.inrialpes.exmo.align.service;
+
+import org.semanticweb.owl.align.Parameters;
+
+/**
+ * Contains the messages that should be sent according to the protocol
+ */
+
+public class Message {
+
+    int surrogate = 0;
+    Message inReplyTo = null;
+    String receiver = "";
+    String sender = "";
+    String content = "";
+    Parameters parameters = null;
+
+    public Message ( int surr, Message rep, String to, String cont, Parameters param ) {
+	surrogate = surr;
+	inReplyTo = rep;
+	receiver = to;
+	content = cont;
+	parameters = param;
+    }
+
+    public int getId () {
+	return surrogate;
+    }
+
+    public Message getInReplyTo() {
+	return inReplyTo;
+    }
+
+    public String getReceiver() {
+	return receiver;
+    }
+
+    public String getContent() {
+	return content;
+    }
+
+    public String getSender() {
+	return sender;
+    }
+
+    public Parameters getParameters() {
+	return parameters;
+    }
+
+}
