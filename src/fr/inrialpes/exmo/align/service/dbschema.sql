7b6f8b019a4a3a1a0c312edb77053355420b4dc0
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2006-10-24 14:45:00 +0000

    - added database creation instructions
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@348 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/service/dbschema.sql
+++ src/fr/inrialpes/exmo/align/service/dbschema.sql
@@ -1,7 +1,28 @@
+#
+# Create the database. If you change any value here, report
+# the changes in the DBInfo.php file.
+#
+
+CREATE DATABASE AServDB;
+
+#
+# Create a MySQL user. Change 'localhost' to the name of the server
+# that hosts MySQL.
+#
+
+GRANT ALL PRIVILEGES ON AServDB.* TO adminAServ@localhost
+       IDENTIFIED BY 'aaa345';
+
+#
+# Create a MySQL user with restricted right for SQL queries
+#
+
+GRANT select ON AServ.*  TO alignserver@localhost IDENTIFIED BY 'adf342';
+
 // alignment info
 
 create table alignment (
-   id varchar(50), 
+   id varchar(100), 
    owlontology1 varchar(100),
    owlontology2 varchar(100),
    type varchar(5),
@@ -15,7 +36,7 @@ create table alignment (
 // cell info
 
 create table cell(
-   id varchar(50),
+   id varchar(100),
    cell_id varchar(20),
    uri1 varchar(100),
    uri2 varchar(100),
@@ -27,6 +48,6 @@ create table cell(
 // cell method
 
 create table method(
-   id varchar(50),
+   id varchar(100),
    tag varchar(20),
    extension varchar(100));
