c77a19f8e006f03157243a3665121e9389d25b7d
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2007-07-18 10:54:57 +0000

    - checked the scripts at ESSOESW 2007
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@523 a7222439-140d-0410-8486-c7398dd066c2

--- html/tutorial/script.bat
+++ html/tutorial/script.bat
@@ -6,81 +6,93 @@ rem #cd alignapi
 rem #unzip align*.zip
 rem #java -jar lib/procalign.jar --help
 rem #cd html/tutorial
-set CWD=/c:/alignapi/html/tutorial
+rem # Angel modifications
+rem #set CWD=/c:/alignapi/html/tutorial
+set CWD=%cd%
+set CWD=/%CWD:\=/%
+set WNDIR=
 
+rem #JE: for finding xsltproc
+set path=c:\3rdparty\bin;%path%
+rem #EndAngel
 
 del results\*.*
-
 
 rem #####################
 rem # Matching
 
-java -jar ../../lib/procalign.jar file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl
+java -jar ../../lib/procalign.jar file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl
 
-java -jar ../../lib/procalign.jar file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/equal.rdf
+java -jar ../../lib/procalign.jar file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\equal.rdf
 
-xsltproc ../form-align.xsl results/equal.rdf > results/equal.html
+xsltproc ../form-align.xsl results\equal.rdf > results\equal.html
 
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/levenshtein.rdf
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\levenshtein.rdf
 
-xsltproc ../form-align.xsl results/levenshtein.rdf > results/levenshtein.html
+xsltproc ../form-align.xsl results\levenshtein.rdf > results\levenshtein.html
 
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/SMOA.rdf
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\SMOA.rdf
 
-xsltproc ../form-align.xsl results/SMOA.rdf > results/SMOA.html
+xsltproc ../form-align.xsl results\SMOA.rdf > results\SMOA.html
 
-rem #java -jar ../../lib/alignwn.jar -i fr.inrialpes.exmo.align.ling.JWNLAlignment file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/jwnl.rdf
-rem #xsltproc ../form-align.xsl results/jwnl.rdf > results/jwnl.html
+java -jar ../../lib/alignwn.jar -Dwndict=%WNDIR% -i fr.inrialpes.exmo.align.ling.JWNLAlignment file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\jwnl.rdf
+
+xsltproc ../form-align.xsl results\jwnl.rdf > results\jwnl.html
 
 rem #####################
 rem # Manipulating
 
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -t 0.33 file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/levenshtein33.rdf
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -t 0.33 file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\levenshtein33.rdf
 
-xsltproc ../form-align.xsl results/levenshtein33.rdf > results/levenshtein33.html
+xsltproc ../form-align.xsl results\levenshtein33.rdf > results\levenshtein33.html
 
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance -t 0.5 file://localhost%CWD%/myOnto.owl file://localhost%CWD%/edu.mit.visus.bibtex.owl -o results/SMOA5.rdf
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance -t 0.5 file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl -o results\SMOA5.rdf
 
-xsltproc ../form-align.xsl results/SMOA5.rdf > results/SMOA5.html
+xsltproc ../form-align.xsl results\SMOA5.rdf > results\SMOA5.html
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter -i results/SMOA5.rdf -o results/AOMS5.rdf
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter -i results\SMOA5.rdf -o results\AOMS5.rdf
 
-xsltproc ../form-align.xsl results/AOMS5.rdf > results/AOMS5.html
+xsltproc ../form-align.xsl results\AOMS5.rdf > results\AOMS5.html
 
 
 rem #####################
 rem # Outputing
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.OWLAxiomsRendererVisitor
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results\SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.OWLAxiomsRendererVisitor
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.SWRLRendererVisitor
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results\SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.SWRLRendererVisitor
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.XSLTRendererVisitor -o results/SMOA5.xsl
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results\SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.XSLTRendererVisitor -o results\SMOA5.xsl
 
-xsltproc results/SMOA5.xsl data.xml > results/data.xml
+xsltproc results\SMOA5.xsl data.xml > results\data.xml
 
 
 rem #####################
 rem # Evaluating
 
-xsltproc ../form-align.xsl refalign.rdf > results/refalign.html
+xsltproc ../form-align.xsl refalign.rdf > results\refalign.html
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://localhost%CWD%/refalign.rdf file://localhost%CWD%/results/equal.rdf
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://%CWD%/refalign.rdf file://%CWD%/results/equal.rdf
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://localhost%CWD%/refalign.rdf file://localhost%CWD%/results/levenshtein33.rdf
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://%CWD%/refalign.rdf file://%CWD%/results/levenshtein33.rdf
 
-rem #java -jar ../../lib/Procalign.jar file://localhost%CWD%/rdf/myOnto.owl file://localhost%CWD%/rdf/edu.mit.visus.bibtex.owl -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -DprintMatrix=1 -o /dev/null > matrix.tex
+rem # This is for printing distance matrix
+rem #java -jar ../../lib/Procalign.jar file://%CWD%/rdf/myOnto.owl file://%CWD%/rdf/edu.mit.visus.bibtex.owl -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -DprintMatrix=1 -o /dev/null > matrix.tex
 
 copy refalign.rdf results
 
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.GroupEval -r refalign.rdf -l "refalign,equal,SMOA,SMOA5,levenshtein,levenshtein33" -c prf -o results/eval.html
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.GroupEval -r refalign.rdf -l "refalign,equal,SMOA,SMOA5,levenshtein,levenshtein33" -c prf -o results\eval.html
 
 
 rem #####################
-rem # Evaluating
+rem # Embedding
 
-javac -classpath ../../lib/api.jar;../../lib/rdfparser.jar;../../lib/align.jar;../../lib/procalign.jar MyApp.java
+javac -classpath ../../lib/api.jar;../../lib/rdfparser.jar;../../lib/align.jar;../../lib/procalign.jar;results -d results Skeleton.java
 
-java -cp ../../lib/Procalign.jar MyApp file://localhost%CWD%/rdf/myOnto.owl file://localhost%CWD%/rdf/edu.mit.visus.bibtex.owl
+java -cp ../../lib/procalign.jar;results Skeleton file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl
+
+javac -classpath ../../lib/api.jar;../../lib/rdfparser.jar;../../lib/align.jar;../../lib/procalign.jar -d results MyApp.java
+
+java -cp ../../lib/procalign.jar;results MyApp file://%CWD%/myOnto.owl file://%CWD%/edu.mit.visus.bibtex.owl
 
 
--- html/tutorial/script.sh
+++ html/tutorial/script.sh
@@ -9,54 +9,81 @@
 #java -jar lib/procalign.jar --help
 #cd html/tutorial
 setenv CWD `pwd`
+setenv WNDIR ../../../WordNet-2.0/dict
 
 /bin/rm results/*
 
 #####################
 # Matching
 
-java -jar ../../lib/procalign.jar file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl
-java -jar ../../lib/procalign.jar file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/equal.rdf
+java -jar ../../lib/procalign.jar file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl
+
+java -jar ../../lib/procalign.jar file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/equal.rdf
+
 xsltproc ../form-align.xsl results/equal.rdf > results/equal.html
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/levenshtein.rdf
+
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/levenshtein.rdf
+
 xsltproc ../form-align.xsl results/levenshtein.rdf > results/levenshtein.html
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/SMOA.rdf
+
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/SMOA.rdf
+
 xsltproc ../form-align.xsl results/SMOA.rdf > results/SMOA.html
-#java -jar ../../lib/alignwn.jar -i fr.inrialpes.exmo.align.ling.JWNLAlignment file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/jwnl.rdf
-#xsltproc ../form-align.xsl results/jwnl.rdf > results/jwnl.html
+
+java -jar ../../lib/alignwn.jar -Dwndict=$WNDIR -i fr.inrialpes.exmo.align.ling.JWNLAlignment file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/jwnl.rdf
+
+xsltproc ../form-align.xsl results/jwnl.rdf > results/jwnl.html
 
 #####################
 # Manipulating
 
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -t 0.33 file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/levenshtein33.rdf
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -t 0.33 file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/levenshtein33.rdf
+
 xsltproc ../form-align.xsl results/levenshtein33.rdf > results/levenshtein33.html
-java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance -t 0.5 file://localhost$CWD/myOnto.owl file://localhost$CWD/edu.mit.visus.bibtex.owl -o results/SMOA5.rdf
+
+java -jar ../../lib/procalign.jar -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=smoaDistance -t 0.5 file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl -o results/SMOA5.rdf
+
 xsltproc ../form-align.xsl results/SMOA5.rdf > results/SMOA5.html
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter -i results/SMOA5.rdf -o results/AOMS5.rdf
+
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -i -o results/AOMS5.rdf
+
 xsltproc ../form-align.xsl results/AOMS5.rdf > results/AOMS5.html
 
 #####################
 # Outputing
 
 java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.OWLAxiomsRendererVisitor
+
 java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.SWRLRendererVisitor
+
 java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.ParserPrinter results/SMOA5.rdf -r fr.inrialpes.exmo.align.impl.renderer.XSLTRendererVisitor -o results/SMOA5.xsl
+
 xsltproc results/SMOA5.xsl data.xml > results/data.xml
 
 #####################
 # Evaluating
 
 xsltproc ../form-align.xsl refalign.rdf > results/refalign.html
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://localhost$CWD/refalign.rdf file://localhost$CWD/results/equal.rdf
-java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://localhost$CWD/refalign.rdf file://localhost$CWD/results/levenshtein33.rdf
-#java -jar ../../lib/Procalign.jar file://localhost$CWD/rdf/myOnto.owl file://localhost$CWD/rdf/edu.mit.visus.bibtex.owl -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -DprintMatrix=1 -o /dev/null > matrix.tex
+
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://$CWD/refalign.rdf file://$CWD/results/equal.rdf
+
+java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.EvalAlign -i fr.inrialpes.exmo.align.impl.eval.PRecEvaluator file://$CWD/refalign.rdf file://$CWD/results/levenshtein33.rdf
+
+# This is for printing distance matrix
+#java -jar ../../lib/Procalign.jar file://$CWD/rdf/myOnto.owl file://$CWD/rdf/edu.mit.visus.bibtex.owl -i fr.inrialpes.exmo.align.impl.method.StringDistAlignment -DstringFunction=levenshteinDistance -DprintMatrix=1 -o /dev/null > matrix.tex
+
 cp refalign.rdf results
+
 java -cp ../../lib/procalign.jar fr.inrialpes.exmo.align.util.GroupEval -r refalign.rdf -l "refalign,equal,SMOA,SMOA5,levenshtein,levenshtein33" -c prf -o results/eval.html
 
 #####################
 # Embedding
 
 javac -classpath ../../lib/api.jar:../../lib/rdfparser.jar:../../lib/align.jar:../../lib/procalign.jar -d results Skeleton.java
-java -cp ../../lib/Procalign.jar:results Skeleton myOnto.owl edu.mit.visus.bibtex.owl
+
+java -cp ../../lib/Procalign.jar:results Skeleton file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl
+
 javac -classpath ../../lib/api.jar:../../lib/rdfparser.jar:../../lib/align.jar:../../lib/procalign.jar -d results MyApp.java
-java -cp ../../lib/Procalign.jar:results MyApp myOnto.owl edu.mit.visus.bibtex.owl > results/MyApp.owl
+
+#///////////// STRANGE RESULTS
+java -cp ../../lib/Procalign.jar:results MyApp file://$CWD/myOnto.owl file://$CWD/edu.mit.visus.bibtex.owl > results/MyApp.owl
