2245226cf7a215b80861eb13220bc8627182bcb1
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2008-02-28 20:19:35 +0000

    - updated Copyright line
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@653 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/impl/rel/SubsumeRelation.java
+++ src/fr/inrialpes/exmo/align/impl/rel/SubsumeRelation.java
@@ -1,6 +1,7 @@
 /*
  * $Id$
- * Copyright (C) INRIA Rhône-Alpes, 2004
+ *
+ * Copyright (C) INRIA Rhône-Alpes, 2004, 2008
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
