2ec82a2868e20f43ea0eb07f763b21bea9c52c02
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2008-04-29 21:27:13 +0000

    - interface for online evaluation
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@717 a7222439-140d-0410-8486-c7398dd066c2

---
+++ src/fr/inrialpes/exmo/align/service/EvalResult.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) INRIA Rhône-Alpes, 2006-2008
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package fr.inrialpes.exmo.align.service;
+
+import org.semanticweb.owl.align.Parameters;
+
+/**
+ * Contains the messages that should be sent according to the protocol
+ */
+
+public class EvalResult extends Success {
+
+    public EvalResult ( int surr, Message rep, String from, String to, String cont, Parameters param ) {
+	super( surr, rep, from, to, cont, param );
+    }
+    public String HTMLString(){
+	return "Alignment ID: <a href=\"../html/retrieve?method=fr.inrialpes.exmo.align.impl.renderer.HTMLRendererVisitor&id="+getContent()+"\">"+getContent()+"</a>";
+    }
+    public String SOAPString(){
+	//return "<id>"+surrogate+"</id>"+"<sender>"+sender+"</sender>" + "<receiver>"+receiver+"</receiver>" + "<in-reply-to>" + inReplyTo+ "</in-reply-to>" + "<alid>" + content + "</alid>";	
+	return "<id>"+surrogate+"</id>"+"<in-reply-to>"+inReplyTo+"</in-reply-to><alid>"+content+"</alid>";	
+    }
+
+}
