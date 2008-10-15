84089154234506ce22931e70bcbce44aa02386bd
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2008-10-15 22:06:24 +0000

    - copyright line updated
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@858 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/util/ParserPrinter.java
+++ src/fr/inrialpes/exmo/align/util/ParserPrinter.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) 2003-2004, 2007-2008 INRIA Rhône-Alpes.
+ * Copyright (C) INRIA, 2003-2004, 2007-2008
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
