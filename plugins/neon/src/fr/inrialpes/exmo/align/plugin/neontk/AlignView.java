297391addbcd4b2346f0de4128988f079808f531
cleduc <cleduc@a7222439-140d-0410-8486-c7398dd066c2>
2008-10-17 08:55:14 +0000

    --renamed class AlignView
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@864 a7222439-140d-0410-8486-c7398dd066c2

---
+++ plugins/neon/src/fr/inrialpes/exmo/align/plugin/neontk/AlignView.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) INRIA Rh�ne-Alpes, 2007-2008
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package fr.inrialpes.exmo.align.plugin.neontk;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.awt.SWT_AWT;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.Frame;
+ 
+public class AlignView extends ViewPart {
+
+	public static final String ID = "fr.inrialpes.exmo.align.plugin.neontk.alignView";
+	 	
+	public void setFocus() {
+	}
+	
+	public void createPartControl(Composite parent) {	
+		final Composite composite = new Composite(parent, SWT.EMBEDDED);
+		final Frame f = SWT_AWT.new_Frame(composite);
+	 
+		JPanel panel = new JPanel(new BorderLayout());
+		f.add(panel);
+		SWTInterface  lo= new SWTInterface();
+		lo.run();
+		f.add(lo);	
+	}
+	
+}
