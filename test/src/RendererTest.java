9a0699d6fd8ba3aaddc649adc196f95eac21de6a
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2009-01-11 12:46:46 +0000

    - one character change in metadata
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@898 a7222439-140d-0410-8486-c7398dd066c2

--- test/src/RendererTest.java
+++ test/src/RendererTest.java
@@ -221,7 +221,7 @@ public class RendererTest {
 	alignment.render( renderer );
 	writer.flush();
 	writer.close();
-	assertEquals( stream.toString().length(), 553, "Rendered differently" );
+	assertEquals( stream.toString().length(), 552, "Rendered differently" );
     }
 
 
