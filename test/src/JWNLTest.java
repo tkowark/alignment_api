4cd334858976e25ccf61e92a84e6b173280f46b1
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2009-07-09 16:39:54 +0000

    - largely moved to ontosim
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1067 a7222439-140d-0410-8486-c7398dd066c2

--- test/src/JWNLTest.java
+++ test/src/JWNLTest.java
@@ -36,7 +36,7 @@ import org.semanticweb.owl.align.Parameters;
 import fr.inrialpes.exmo.align.impl.renderer.RDFRendererVisitor;
 import fr.inrialpes.exmo.align.impl.BasicParameters;
 import fr.inrialpes.exmo.align.ling.JWNLAlignment;
-import fr.inrialpes.exmo.align.ling.JWNLDistances;
+import fr.inrialpes.exmo.ontosim.string.JWNLDistances;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
@@ -99,70 +99,6 @@ $ java -jar lib/alignwn.jar -D=$WNDIR file://$CWD/examples/rdf/ file://$CWD/exam
 	alignment.cut( "hard", 0.4 );
 	assertEquals( alignment.nbCells(), 10 );
 
-    }
-
-    // This was in the class "main"
-    @Test(groups = { "full", "ling" }, dependsOnMethods = {"routineInitializeWordNet"})
-    public void routineJWNLDistanceTest() throws Exception {
-
-	assertEquals( jDist.compareComponentNames( "French997Guy", "Dutch_Goa77ly" ), 0.23684210526315788);
-	assertEquals( jDist.compareComponentNames( "FREnch997guy21GUIe", "Dutch_GOa77ly." ), 0.09795918367346938);
-	assertEquals( jDist.compareComponentNames( "a997c", "77ly."), .0);
-	assertEquals( jDist.compareComponentNames( "MSc", "PhD"), 0.2777777777777778);
-    }
-
-    @Test(groups = { "full", "ling" })
-    public void routineJWNLWuPalmerTest() throws Exception {
-
-	// In comment the values in our book (1st ed. p102; computed with WordNet 2.0)
-	assertEquals( jDist.wuPalmerSimilarity( "author", "author" ), 1.);//1.
-	assertEquals( jDist.wuPalmerSimilarity( "author", "writer" ), 1.); //1.
-	assertEquals( jDist.wuPalmerSimilarity( "author", "illustrator" ), .6);//.5
-	assertEquals( jDist.wuPalmerSimilarity( "author", "creator" ), .75);//.67
-	assertEquals( jDist.wuPalmerSimilarity( "author", "Person" ), .8);//.4
-    }
-
-    @Test(groups = { "full", "ling" })
-    public void basicSynonymDistanceTest() throws Exception {
-
-	assertEquals( jDist.basicSynonymDistance( "author", "author" ), 0.);//
-	assertEquals( jDist.basicSynonymDistance( "author", "writer" ), 0.); //
-	assertEquals( jDist.basicSynonymDistance( "author", "illustrator" ), .5);//
-	assertEquals( jDist.basicSynonymDistance( "author", "creator" ), .5);//
-	assertEquals( jDist.basicSynonymDistance( "author", "Person" ), 0.6666666666666667);//
-    }
-
-    @Test(groups = { "full", "ling" })
-    public void cosynonymySimilarityTest() throws Exception {
-
-	// In comment the values in our book (1st ed. p89)
-	assertEquals( jDist.cosynonymySimilarity( "author", "author" ), 1.);//1.
-	assertEquals( jDist.cosynonymySimilarity( "author", "writer" ), .25); //.25
-	assertEquals( jDist.cosynonymySimilarity( "author", "illustrator" ), 0.);//0.
-	assertEquals( jDist.cosynonymySimilarity( "author", "creator" ), 0.);//0.
-	assertEquals( jDist.cosynonymySimilarity( "author", "Person" ), 0.);//0.
-    }
-
-    @Test(groups = { "full", "ling" })
-    public void basicSynonymySimilarityTest() throws Exception {
-
-	// In comment the values in our book (1st ed. p89)
-	assertEquals( jDist.basicSynonymySimilarity( "author", "author" ), 1.);//1.
-	assertEquals( jDist.basicSynonymySimilarity( "author", "writer" ), 1.); //1.
-	assertEquals( jDist.basicSynonymySimilarity( "author", "illustrator" ), 0.);//0.
-	assertEquals( jDist.basicSynonymySimilarity( "author", "creator" ), 0.);//0.
-	assertEquals( jDist.basicSynonymySimilarity( "author", "Person" ), 0.);//0.
-    }
-
-    @Test(groups = { "full", "ling" })
-    public void computeSimilarityTest() throws Exception {
-
-	// In comment the values in our book
-	assertEquals( jDist.computeSimilarity( "author", "author" ), 1.);//
-	assertEquals( jDist.computeSimilarity( "author", "writer" ), 1.); //
-	assertEquals( jDist.computeSimilarity( "author", "illustrator" ), .8);//
-	assertEquals( jDist.computeSimilarity( "author", "creator" ), 0.8888888888888888);//
-	assertEquals( jDist.computeSimilarity( "author", "Person" ), 0.875);//
     }
 
 }
