8aaf6e015d49c83bc42bb4775089f2706c9be962
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2009-09-22 12:58:59 +0000

    - added a test examplifying incorrect cut behaviour
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1153 a7222439-140d-0410-8486-c7398dd066c2

--- test/src/BasicAlignmentTest.java
+++ test/src/BasicAlignmentTest.java
@@ -22,6 +22,7 @@
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertNull;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Configuration;
@@ -29,8 +30,11 @@ import org.testng.annotations.Test;
 //import org.testng.annotations.*;
 
 import fr.inrialpes.exmo.align.impl.BasicAlignment;
+import fr.inrialpes.exmo.align.impl.URIAlignment;
+import fr.inrialpes.exmo.align.parser.AlignmentParser;
 
 import org.semanticweb.owl.align.Alignment;
+import org.semanticweb.owl.align.AlignmentException;
 
 public class BasicAlignmentTest {
     private Alignment alignment = null;
@@ -44,5 +48,19 @@ public class BasicAlignmentTest {
     public void aFastTest() {
 	assertNotNull( alignment, "Alignment was null" );
     }
+
+    @Test(groups = { "full", "raw" })
+    public void someCutTest() throws AlignmentException {
+	// THIS SHOULD BE REPLACED WITH ALIGNMENT BUILT IN PREVIOUS TESTS
+	AlignmentParser aparser = new AlignmentParser( 0 );
+	assertNotNull( aparser, "AlignmentParser was null" );
+	Alignment result = aparser.parse( "file:examples/rdf/newsample.rdf" );
+	assertNotNull( result, "URIAlignment(result) was null" );
+	assertTrue( result instanceof URIAlignment );
+	assertEquals( result.nbCells(), 2, "Alignment should contain 2 cells" );
+	result.cut( "hard", .5 );
+	assertEquals( result.nbCells(), 1, "Alignment should contain 1 cell" );
+    }
+
 
 }
