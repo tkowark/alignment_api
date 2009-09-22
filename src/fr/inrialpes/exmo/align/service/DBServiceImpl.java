9c1cc52afde66e40c01061e073ab18e35481eca0
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2009-09-22 21:27:24 +0000

    - updated format - edited copyright
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1162 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/service/DBServiceImpl.java
+++ src/fr/inrialpes/exmo/align/service/DBServiceImpl.java
@@ -2,7 +2,7 @@
  * $Id$
  *
  * Copyright (C) Seungkeun Lee, 2006
- * Copyright (C) INRIA Rhône-Alpes, 2007-2008
+ * Copyright (C) INRIA, 2007-2009
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
