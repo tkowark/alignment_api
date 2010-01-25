e64513fa41a4603cc30045e7799377cc6c1986f2
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2010-01-25 10:20:03 +0000

    - Added new extended tutorial
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1213 a7222439-140d-0410-8486-c7398dd066c2

---
+++ html/tutorial/tutorial3/MyAlignment.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) INRIA, 2010
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+//package my.domain;
+
+import org.semanticweb.owl.align.Alignment;
+import org.semanticweb.owl.align.AlignmentProcess; 
+import org.semanticweb.owl.align.AlignmentException; 
+
+import fr.inrialpes.exmo.align.impl.BasicAlignment; 
+
+//import my.domain.MyMatcher; 
+
+import java.lang.Double;
+import java.util.Properties;
+import java.net.URI;
+
+public class MyAlignment extends BasicAlignment implements AlignmentProcess {
+
+    public MyAlignment() {};
+     
+    /* init( onto1, onto2 ) is inherited */
+
+    public void align( Alignment alignment, Properties params ) throws AlignmentException {
+
+	URI url1 = getFile1();
+	URI url2 = getFile2();
+
+	MyMatcher matcher = new MyMatcher();
+	matcher.match( url1, url2 ); 
+
+	for ( Object[] c : matcher ){
+	    addAlignCell( (URI)c[0], (URI)c[1], (String)c[2], ((Double)c[3]).doubleValue() );
+	} 
+    } 
+}
+
+ 
