16c1c7bda09fc9c7ae3b1c9fba1deeed083a033f
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2010-02-13 15:01:50 +0000

    - implemented in the most simple way
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1237 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/impl/method/EditDistNameAlignment.java
+++ src/fr/inrialpes/exmo/align/impl/method/EditDistNameAlignment.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) INRIA 2003-2005, 2007-2009
+ * Copyright (C) INRIA 2003-2005, 2007-2010
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -22,11 +22,6 @@ package fr.inrialpes.exmo.align.impl.method;
 
 import org.semanticweb.owl.align.AlignmentProcess;
 
-import fr.inrialpes.exmo.align.impl.DistanceAlignment;
-import fr.inrialpes.exmo.align.impl.MatrixMeasure;
-
-import fr.inrialpes.exmo.ontosim.string.StringDistances;
-
 /**
  * This class aligns ontology with regard to the editing distance between 
  * class names.
@@ -36,31 +31,11 @@ import fr.inrialpes.exmo.ontosim.string.StringDistances;
  * @version $Id$ 
  */
 
-public class EditDistNameAlignment extends DistanceAlignment implements AlignmentProcess {
+public class EditDistNameAlignment extends StringDistAlignment implements AlignmentProcess {
+
     /** Creation **/
     public EditDistNameAlignment(){
-	setSimilarity( new MatrixMeasure() {
-		public double measure( Object o1, Object o2 ) throws Exception {
-		    String s1 = ontology1().getEntityName( o1 );
-		    String s2 = ontology2().getEntityName( o2 );
-		    if ( s1 == null || s2 == null ) return 1.;
-		    else return StringDistances.levenshteinDistance(
-							s1.toLowerCase(),
-							s2.toLowerCase()) / max(s1.length(),s2.length());
-		}
-		public double classMeasure( Object cl1, Object cl2 ) throws Exception {
-		    return measure( cl1, cl2 );
-		}
-		public double propertyMeasure( Object pr1, Object pr2 ) throws Exception {
-		    return measure( pr1, pr2 );
-		}
-		public double individualMeasure( Object id1, Object id2 ) throws Exception {
-		    return measure( id1, id2 );
-		}
-	    } );
+	methodName = "levenshteinDistance";
     };
-
-    private double max( double i, double j) { if ( i>j ) return i; else return j; }
-
 }
 
