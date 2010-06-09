5a5ae6b1e793b2875610849028c89614443e7eca
trojahnd <trojahnd@a7222439-140d-0410-8486-c7398dd066c2>
2010-06-09 14:01:13 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1453 a7222439-140d-0410-8486-c7398dd066c2

---
+++ html/tutorial/tutorial5/MyAlignmentWS.java
@@ -0,0 +1,42 @@
+package eu.sealsproject.omt.ws.matcher;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.util.Properties;
+
+import javax.jws.WebService;
+
+import org.semanticweb.owl.align.Alignment;
+import org.semanticweb.owl.align.AlignmentException;
+import org.semanticweb.owl.align.AlignmentProcess;
+import org.semanticweb.owl.align.AlignmentVisitor;
+
+import fr.inrialpes.exmo.align.impl.renderer.RDFRendererVisitor;
+
+@WebService(endpointInterface="eu.sealsproject.omt.ws.matcher.AlignmentWS")
+public class MyAlignmentWS extends MyAlignment implements AlignmentWS {
+
+	@Override
+	public String align(URI source, URI target) {
+		   try {
+			   init(source,target);
+			   align((Alignment)null, new Properties());
+			   SBWriter sbWriter = null;
+			   try {
+					sbWriter = new SBWriter(new BufferedWriter(new OutputStreamWriter( System.out, "UTF-8" )), true);
+					AlignmentVisitor renderer = new RDFRendererVisitor(sbWriter);
+					render(renderer);
+		                        String alignment = sbWriter.toString();
+					return alignment;
+				}
+			   catch(Exception e) { }
+			   
+		   } catch (AlignmentException e) {
+			 		e.printStackTrace();
+		   }
+		   return null;
+	}
+
+}
