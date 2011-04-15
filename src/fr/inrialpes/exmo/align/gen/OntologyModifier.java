d2c2c4887d569057f8eac9a2aafc457d1a2e4bcd
rosoium <rosoium@a7222439-140d-0410-8486-c7398dd066c2>
2011-04-15 08:36:17 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1560 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/gen/OntologyModifier.java
+++ src/fr/inrialpes/exmo/align/gen/OntologyModifier.java
@@ -55,9 +55,11 @@ import com.hp.hpl.jena.vocabulary.OWL;
 import com.hp.hpl.jena.vocabulary.RDFS;
 import com.hp.hpl.jena.vocabulary.XSD;
 
+/*
 //WordNet API classes
 import edu.smu.tspell.wordnet.Synset;
 import edu.smu.tspell.wordnet.WordNetDatabase;
+*/
 
 
 //activeRandomString is true -> we replace the label with a random string
