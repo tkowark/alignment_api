4e9de2c17fef576ea12425e74989d7207bcd5f6e
euzenat <euzenat@a7222439-140d-0410-8486-c7398dd066c2>
2012-02-15 12:17:28 +0000

    - cleaned up import lists
    git-svn-id: https://scm.gforge.inria.fr/svn/alignapi/trunk@1677 a7222439-140d-0410-8486-c7398dd066c2

--- src/fr/inrialpes/exmo/align/service/HTMLAServProfile.java
+++ src/fr/inrialpes/exmo/align/service/HTMLAServProfile.java
@@ -70,19 +70,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.Request;
-
-import org.mortbay.util.MultiMap;
-import org.mortbay.util.StringUtil;
-import org.mortbay.util.TypeUtil;
-
-import org.mortbay.jetty.handler.DefaultHandler;
-import org.mortbay.jetty.handler.AbstractHandler;
-import org.mortbay.jetty.handler.HandlerList;
-import org.mortbay.jetty.handler.ResourceHandler;
 import org.mortbay.servlet.MultiPartFilter;
-import org.mortbay.jetty.servlet.Context.SContext;
 
 /**
  * HTMLAServProfile: an HTML provile for the Alignment server
